2c1cd4e83ed55820ada5e9e896ad7266e7b5f9de
harinath kanchu <harinathkanchu@harinath001.local>
2018-12-09 15:54:28 -0500

    cleaning - 1

--- entropy_estC.py
+++ entropy_estC.py
@@ -1,158 +1,138 @@
 import numpy as np
-from utilities.utilities import find,greater
 import math
 from scipy.stats import poisson
 from scipy.optimize import linprog
-import pdb
 
-def entropy_estC(f):
-    # Input: fingerprint f, where f(i) represents number of elements that
-    # appear i times in a sample.  Thus sum_i i*f(i) = sample size.
-    # File makeFinger.m transforms a sample into the associated fingerprint.
-    #
-    # Output: approximation of 'histogram' of true distribution.  Specifically,
-    # histx(i) represents the number of domain elements that occur with
-    # probability x(i).   Thus sum_i x(i)*histx(i) = 1, as distributions have
-    # total probability mass 1.
-    #
-    # An approximation of the entropy of the true distribution can be computed
-    # as:    Entropy = (-1)*sum(histx.*x.*log(x))
 
-    f = f.T.flatten()
-    k = np.matmul(f, np.array(range(1, f.shape[0]+1)).T)
+def entropy_est(f):
+    # Calculation of Sample Size
+    f = np.transpose(f).flatten()
+    k = np.array(range(1, len(f) + 1))
+    k = np.dot(f, k)
 
+    # Variable definitions
     gridFactor = 1.05
     alpha = 0.5
-    xLPmin = 1.00/float((k*max(10, k)))
-    min_i = min(find(greater(f, 0)))
+    xLPmin = 1.0 / float((k * max(10, k)))
+    min_i = min([i for i in range(len(f)) if f[i] > 0])
     if min_i > 0:
-        xLPmin = float(min_i)/float(k)
+        xLPmin = float(min_i / (k * 1.0))
     maxLPIters = 1000
 
+    x = list()
+    histx = list()
+    fLP = np.zeros(len(f))
 
-    x = 0
-    histx = 0
-    fLP = [0]*len(f)
-    for i in range(0, len(f)):
-        if f[i]>0:
-            wind = [ max(0, i-math.ceil(math.sqrt(i))), min(i+math.ceil(math.sqrt(i)), len(f))]
-            if sum(f[int(wind[0]):int(wind[1])+1]) < math.sqrt(i):
-                x = [x, i/k]
-                histx = [histx, f[i]]
+    for i in range(len(f)):
+        if f[i] > 0:
+            window = [max(0, i - math.ceil(math.sqrt(i))), min(i + math.ceil(math.sqrt(i)), len(f))]
+            window = [int(window[0]), int(window[1])]
+
+            if np.sum(f[window[0]:window[1]]) < math.sqrt(i):
+                x.append(float(i / (k * 1.0)))
+                histx.append(float(f[i]))
                 fLP[i] = 0
             else:
                 fLP[i] = f[i]
 
-    fmax = max(find(greater(fLP, 0)))
-    if not fmax:
-        x = x[1:]
-        histx = histx[1:]
-        ent = -np.matmul(histx, np.array([y*math.log(y) for y in x]).T) + sum(histx)/(2*k)
+    fmax = [i for i in range(len(f)) if f[i] > 0]
+    if len(fmax) == 0:
+        ent = (-1) * (np.dot(histx, [y * math.log(y) for y in x])) + float(np.sum(histx)) / (2.0 * k)
         return ent
-    LPmass = 1 - np.matmul(np.array([x]), np.array([histx]).T)
-    fLP = fLP[0:fmax+1] + [0]*int(math.ceil(math.sqrt(fmax))) # assuming list append here
+    else:
+        fmax = max(fmax) + 1
+
+    LPmass = 1.0 - np.dot(x, histx)
+
+    fLP = np.concatenate((fLP[:fmax], np.zeros(int(math.ceil(math.sqrt(fmax))))))
     szLPf = len(fLP)
 
-    xLPmax = float(fmax)/float(k)
-    xLP = xLPmin*(np.array([gridFactor**x for x in range(0, int(math.ceil(math.log(float(xLPmax)/float(xLPmin))/math.log(gridFactor))+1 ))]))
-    # convert xLP round to 4 as default in matlab
-    xLP = np.array([round(x, 4) for x in xLP])
+    xLPmax = float(fmax / (k * 1.0))
+    temp = [gridFactor ** i for i in
+            range(0, int(math.ceil(math.log((xLPmax * 1.0) / xLPmin) / math.log(gridFactor)) + 1))]
+    xLP = np.array(temp)
+    xLP = xLPmin * xLP
     szLPx = len(xLP)
 
-    objf = np.zeros(szLPx+2*szLPf).reshape(szLPx+2*szLPf, 1)
-    sqrt_fLP = np.array([1.0/math.sqrt(x+1) for x in fLP])
-    sqrt_fLP = sqrt_fLP.reshape(len(sqrt_fLP), 1)
+    objf = np.zeros((szLPx + 2 * szLPf, 1))
+    for i in range(szLPx, len(objf), 2):
+        objf[i] = 1.0 / math.sqrt(fLP[(i - szLPx) / 2] + 1)
+        objf[i + 1] = 1.0 / math.sqrt(fLP[(i - szLPx) / 2] + 1)
 
-    # objf[szLPx:] = sqrt_fLP*len(objf[szLPx:]) # converting to
-    objf[szLPx::2] = sqrt_fLP
-    objf[szLPx+1::2] = sqrt_fLP
+    A = np.zeros((2 * szLPf, szLPx + 2 * szLPf))
+    b = np.zeros((2 * szLPf, 1))
 
-    A = np.zeros(2*szLPf*(szLPx+2*szLPf)).reshape(2*szLPf, szLPx+2*szLPf)
-    b = np.zeros(2*szLPf).reshape(2*szLPf, 1)
+    for i in range(szLPf):
+        A[2 * i][:szLPx] = poisson.pmf(i + 1, k * xLP)
+        A[(2 * i) + 1][:szLPx] = (-1) * A[2 * i][:szLPx]
+        A[2 * i][szLPx + (2 * i)] = -1
+        A[(2 * i) + 1][szLPx + (2 * i) + 1] = -1
+        b[2 * i] = fLP[i]
+        b[(2 * i) + 1] = (-1) * fLP[i]
 
-    for i in range(0, szLPf):
-        A[2*i][:szLPx] = poisson.pmf(i+1, k*xLP) # [pmf]*times
-        A[2*i+1][:szLPx] = (-1)*A[2*i][:szLPx]
-        A[2*i][szLPx+2*i] = -1
-        A[2*i+1][szLPx+2*i+1] = -1
-        b[2*i] = fLP[i]
-        b[2*i+1] = 0-fLP[i]
-
-    Aeq = np.zeros(szLPx+2*szLPf).reshape(1, szLPx+2*szLPf)
-    Aeq[0][:szLPx] = xLP.reshape(1, len(xLP)) # Aeq is row matrix so replace the first row first szLPx elements to xLP elements
-    beq = LPmass
+    Aeq = np.zeros((1, szLPx + 2 * szLPf))
+    # Aeq[0:szLPx][0] = xLP
+    for i in range(szLPx):
+        Aeq[0][i] = xLP[i]
+    beq = np.zeros((1, 1))
+    beq[0][0] = LPmass
 
     for i in range(szLPx):
-        A[:,i] /= xLP[i]
-        Aeq[0][i] /= xLP[i]
+        Aeq[0][i] = Aeq[0][i] / xLP[i]
+        A[:, i] = A[:, i] / xLP[i]
 
     options = {"maxiter": maxLPIters, "disp": False}
-    # pdb.set_trace()
-    pdb.set_trace()
-    result1 = linprog(np.squeeze(objf), A, b, Aeq, beq, options=options)
+
+    A = np.around(A, 4)
+    b = np.around(b, 4)
+    Aeq = np.around(Aeq, 4)
+    objf = np.around(objf, 4)
+
+    result1 = linprog(np.squeeze(objf), A, b, Aeq, beq, method="interior-point", options=options)
 
     exitflag = result1["status"]
     fval = result1["fun"]
-    sol = result1["x"]
     if exitflag == 1:
-        print 'maximum number of iterations reached--try increasing maxLPIters'
+        print('maximum number of iterations reached--try increasing maxLPIters')
     elif exitflag > 1:
-        print 'LP1 solution was not found, still solving LP2 anyway...', exitflag
+        print('LP1 solution was not found, still solving LP2 anyway...', exitflag)
 
+    if min_i < 1:
+        objf2 = np.zeros((szLPx + 2 * szLPf, 1))
+        objf2[:szLPx] = 1
+        A2 = np.vstack((A, np.transpose(objf)))
+        b2 = np.vstack((b, np.array(fval + alpha)))
 
-
-    #106
-    # change
-    if min_i < 2:
-        objf2 = np.zeros(objf.shape)
-        objf2[0:szLPx] = 1
-        A2 = np.vstack((A, objf.T))
-        b2 = np.vstack((b,fval+alpha))
         for i in range(szLPx):
-            objf2[i] /= xLP[i]
+            objf2[i] = objf2[i] / xLP[i]
 
-        #116 sol2,fval2 = linprog?
-        # [sol2, fval2, exitflag2, output] = linprog(objf2, A2, b2, Aeq, beq, zeros(szLPx+2*szLPf,1), Inf*ones(szLPx+2*szLPf,1),[], options);
-        result2 = linprog(np.squeeze(objf2), A2, b2, Aeq, beq, options=options)
-        fval2 = result2["fun"]
-        exitflag2 = result2["status"]
-        sol2 = result2["x"]
+        A2 = np.around(A2, 4)
+        b2 = np.around(b2, 4)
+        Aeq = np.around(Aeq, 4)
+        objf2 = np.around(objf2, 4)
 
-        if exitflag2 != 0:
-            print "LP2 solution was not found", exitflag2
+        result2 = linprog(np.squeeze(objf2), A2, b2, Aeq, beq, method="interior-point", options=options)
+
+        exitflag = result2["status"]
+        if exitflag > 1:
+            print('LP2 solution was not found', exitflag)
+        sol2 = result2['x']
     else:
-        # change
-        sol2 = sol
+        sol2 = result1['x']
 
     sol2[0:szLPx] = np.divide(sol2[0:szLPx], xLP)
+    sol2 = np.around(sol2, 4)
 
-
-    # change
-    if len(np.where(x > 0)[0]) == 0:
-        ent = -np.matmul(sol2[0:szLPx].T , np.array([x*math.log(x) for x in xLP]))
+    fmax1 = [i for i in range(len(x)) if x[i] > 0]
+    if len(fmax1) == 0:
+        ent = (-1) * (np.dot(sol2[:szLPx], [z * math.log(z) for z in xLP]))
     else:
-        ent = - np.matmul(histx * (np.where(x>0)),     ((x*np.where(x>0)) * (np.array([math.log(y) for y in np.where(x>0)]))).T ) \
-        + ( sum(histx[np.where(x>0)]) ) / (2*k - np.matmul(sol2[1:szLPx].T, (xLP*np.array([math.log(y) for y in x])).T  ) )
-
+        histx = np.array(histx)
+        x = np.array(x)
+        x1 = x[fmax1]
+        ent = (-1) * np.dot(histx[fmax1], [z * math.log(z) for z in x1])
+        ent += np.sum(histx[fmax1]) / (2.0 * k)
+        ent -= np.dot(sol2[:szLPx], [z * math.log(z) for z in xLP])
+    # ent = (-1)*np.matmul(histx * (np.where(x>0)),((x*np.where(x>0)) * (np.array([math.log(y) for y in np.where(x>0)]))).T ) + ( sum(histx[np.where(x>0)]) ) / (2*k - np.matmul(sol2[1:szLPx].T, (xLP*np.array([math.log(y) for y in x])).T  ) )
 
     return ent
-
-    # x = np.hstack((x,xLP))
-    # histx = np.hstack((histx,sol2.T))
-    # x.sort()
-    # ind = x.argsort()
-    # histx = histx[ind]
-    # ind = np.where(histx > 0)
-    # x = x[ind]
-    # histx = histx[ind]
-    # return [histx, x]
-
-
-from makeFinger import makeFinger
-from unseen import unseen
-inp = makeFinger([5, 6, 9, 20, 19, 12, 12, 2, 12, 18])
-print(inp)
-print(unseen(inp))
-print(entropy_estC(inp))
-
-
